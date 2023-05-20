17f7e5eb98bef3ec61df693a2554e4d814bd17ea
polyaxon-ci <ci@polyaxon.com>
2023-05-20 14:05:55 +0200

    Fix random state generator

--- hypertune/hypertune/search_managers/utils.py
+++ hypertune/hypertune/search_managers/utils.py
@@ -2,4 +2,4 @@ import numpy as np
 
 
 def get_random_generator(seed=None):
-    return np.random.RandomState(seed) if seed else np.random
+    return np.random.RandomState(seed) if seed else np.random.RandomState()
