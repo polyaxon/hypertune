243e4d9e603a25edecce1ef915949f36a4b5e0de
polyaxon-ci <ci@polyaxon.com>
2023-05-26 13:18:46 +0100

    Remove eager mode

--- hypertune/tests/test_kinds.py
+++ hypertune/tests/test_kinds.py
@@ -9,13 +9,6 @@ class TestKinds(BaseTestCase):
     def test_supported_kinds(self):
         assert len(V1MatrixKind.to_list()) == 7
 
-    def test_eager_kinds(self):
-        assert V1MatrixKind.eager_values() == {
-            V1MatrixKind.MAPPING,
-            V1MatrixKind.GRID,
-            V1MatrixKind.RANDOM,
-        }
-
     def test_iteration_values(self):
         assert V1MatrixKind.iteration_values() == {
             V1MatrixKind.HYPERBAND,
