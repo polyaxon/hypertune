eeed6e25ca42949e4f4a60e9fda189eb27a17817
polyaxon-ci <ci@polyaxon.com>
2023-06-05 11:55:42 +0200

    Bump version to v1.1.0-rc17

--- hypertune/hypertune/pkg.py
+++ hypertune/hypertune/pkg.py
@@ -1,5 +1,5 @@
 NAME = "hypertune"
-VERSION = "1.1.0-rc16"
+VERSION = "1.1.0-rc17"
 DESC = "A library for performing hyperparameter optimization with Polyaxon."
 URL = "https://github.com/polyaxon/hypertune"
 AUTHOR = "Polyaxon, Inc."
